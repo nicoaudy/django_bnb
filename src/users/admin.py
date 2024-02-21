60831489e7a240701736747975c6bbdc2812dd40
Nico Audy <nicoaudy1994@gmail.com>
2024-02-21 16:51:34 +0700

    using useradmin fieldsets

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
 
     """ custom user admin"""
 
-    list_display = ("username", "email", "gender", "language", "currency", "superhost",)
-    list_filter = ("superhost", "language", "currency",)
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthday",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            }
+        ),
+    )
 
